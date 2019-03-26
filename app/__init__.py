3feaea0f62b142c0e46f54daa89809fa5f0620d2
Durga Sai <konurud@gmail.com>
2019-03-26 07:00:18 +0530

    speech.py added

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,14 @@
 
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 # application object as an instance of class Flask
 app = Flask(__name__) # name is a predefined variable
 app.config.from_object(Config)
+db=SQLAlchemy(app)
+migrate=Migrate(app, db)
 
 # importing the route module
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 # HOME PAGE ROUTE
 
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 # routes are the different URLs that the application implements
 @app.route('/') # decorator modifies the following function
@@ -19,3 +20,11 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Log In', form=form)
